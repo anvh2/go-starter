a3c1f19bc6440de00b054970663e1e2277bafb11
taitt <taitt@vng.com.vn>
2018-05-25 12:01:52 +0700

    Add deploy gae

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+		c.String(200, "pung")
 	})
 	return r
 }
