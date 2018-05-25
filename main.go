2a1bfdde99ec0cb026af2b6943a4643b6f94e87b
taitt <taitt@vng.com.vn>
2018-05-25 12:57:06 +0700

    Update travis

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "github.com/gin-gonic/gin"
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pung")
+		c.String(200, "pang")
 	})
 	return r
 }
--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -6,14 +6,12 @@ set -e
 
 echo "mode: atomic" > coverage.txt
 
-for d in $(find ./* -maxdepth 10 -type d); do
-    if ls $d/*.go &> /dev/null; then
-        go test  -coverprofile=profile.out -covermode=atomic $d
-        if [ -f profile.out ]; then
-            echo "$(pwd)"
-            cat profile.out | grep -v "mode: " >> coverage.txt
-            rm profile.out
-        fi
+for d in $(go list ./...); do
+    go test  -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        echo "$(pwd)"
+        cat profile.out | grep -v "mode: " >> coverage.txt
+        rm profile.out
     fi
 done
 
