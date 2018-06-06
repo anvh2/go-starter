9c4e0bd29bd9316cd6a7e0591fd6a6a0bb3b267f
taitt <taitt@vng.com.vn>
2018-06-06 14:33:40 +0700

    Add viper

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/fsnotify/fsnotify"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func setupRouter() *gin.Engine {
@@ -14,6 +18,20 @@ func setupRouter() *gin.Engine {
 }
 
 func main() {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.AddConfigPath(".")      // optionally look for config in the working directory
+	err := viper.ReadInConfig()   // Find and read the config file
+	if err != nil {               // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s", err))
+	}
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Config file changed:", e.Name)
+	})
+
+	println(viper.GetString("owner.name"))
+
 	gin.SetMode(gin.ReleaseMode)
 	// i, _ := testErr()
 	// println(i)
